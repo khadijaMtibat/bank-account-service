8e182ba60e05ad7f4876cb1a7a65fae9bca42f8e
Khadija Mtibat <mtibatkhadija1@gmail.com>
2024-01-28 19:18:04 +0100

    first commit

---
+++ src/main/java/org/sid/bankaccountservice/repositories/BankAccountRepository.java
@@ -0,0 +1,19 @@
+package org.sid.bankaccountservice.repositories;
+
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.enums.AccountType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource //demarer un web service REST FULL
+public interface BankAccountRepository  extends JpaRepository<BankAccount, String> {
+
+    @RestResource(path="/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
+
+}
